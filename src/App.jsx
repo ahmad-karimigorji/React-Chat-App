d384f1d2aae09f158f160fb9731ed613d0129350
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2023-05-27 17:02:40 +0330

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./component/NavBar";
+import Login from "./pages/Login";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Login />
+    </div>
+  );
+}
+
+export default App;
