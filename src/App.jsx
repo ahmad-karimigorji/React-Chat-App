559118d8085d585492d5f769deedf4b5527c3180
ahmad-karimigorji <ahmadkarimigorji@yahoo.com>
2023-06-01 18:14:00 +0330

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,38 @@
-import Navbar from "./component/NavBar";
+import PrivateRoute from "./routes/PrivateRoute";
+import { AuthProvider } from "./context/AuthContext";
+import ChatRoom from "./pages/ChatRoom";
+import Layout from "./pages/Layout";
 import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    // errorElement: <Layout outlet={<ErrorPage />} />,
+    children: [
+      { index: true, element: <Login /> },
+      {
+        path: "chat",
+        element: (
+          <PrivateRoute>
+            <ChatRoom />
+          </PrivateRoute>
+        ),
+      },
+      { path: "signup", element: <Signup /> },
+    ],
+  },
+]);
 
 function App() {
   return (
-    <div>
-      <Navbar />
-      <Login />
-    </div>
+    <>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </>
   );
 }
 
